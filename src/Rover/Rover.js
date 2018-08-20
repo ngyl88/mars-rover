e5d8d43d68264412e6ef20548d2020dc09066522
ngyl88 <ng_yl88@hotmail.com>
2018-08-20 14:46:25 +0800

    Rover constructor

--- src/Rover/Rover.js
+++ src/Rover/Rover.js
@@ -1,3 +1,9 @@
 class Rover {
-    
+  constructor(x, y, orientation) {
+    this.x = x;
+    this.y = y;
+    this.orientation = orientation;
+  }
 }
+
+module.exports = Rover;
--- src/Rover/Rover.test.js
+++ src/Rover/Rover.test.js
@@ -1,3 +1,11 @@
-it('smoke test', () => {
-    expect(1).toBe(1);
+const Rover = require('./Rover');
+
+it("Rover constructor", () => {
+    const rover = new Rover(0, 0, 'N');
+    expect(rover.x).toBe(0);
+    expect(rover.y).toBe(0);
+    expect(rover.orientation).toBe('N');
+});
+
+describe.skip("L/R instructions", () => {
 });
