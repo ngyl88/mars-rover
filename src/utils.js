97c73282a9319018d5a09bc2515f4c4df50271fc
ngyl88 <ng_yl88@hotmail.com>
2018-08-20 19:52:27 +0800

    utils: isValidInitialPositionInput

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+const orientationObject = require("./orientation");
+
 const readInstructionsToArray = string => {
   return string.split("");
 };
@@ -8,14 +10,33 @@ const isValidCoordinateInput = inputString => {
     return false;
   }
 
-  inputArray.forEach(input => {
-    const parsed = Number.parseInt(input);
-    if (Number.isNaN(parsed)) return false;
-  });
+  const x = Number.parseInt(inputArray[0]);
+  const y = Number.parseInt(inputArray[1]);
+
+  if (isNaN(x) || isNaN(y)) {
+    return false;
+  }
   return true;
 };
 
+const isValidOrientation = orientation => {
+  const validOrientation = Object.values(orientationObject);
+  return validOrientation.indexOf(orientation) !== -1;
+}
+
+const isValidInitialPositionInput = positionString => {
+  const inputArray = positionString.split(" ");
+  if (inputArray.length != 3) {
+    return false;
+  }
+
+  const coordinates = positionString.slice(0, positionString.lastIndexOf(' '));
+  if(!isValidCoordinateInput(coordinates)) return false;
+  return isValidOrientation(inputArray[2]);
+}
+
 module.exports = {
   readInstructionsToArray,
-  isValidCoordinateInput
+  isValidCoordinateInput,
+  isValidInitialPositionInput
 };
--- src/utils.test.js
+++ src/utils.test.js
@@ -1,4 +1,4 @@
-const { readInstructionsToArray, isValidCoordinateInput } = require("./utils");
+const { readInstructionsToArray, isValidCoordinateInput, isValidInitialPositionInput } = require("./utils");
 
 describe("readInstructionsToArray", () => {
   it("should split a series of instructions into each instruction", () => {
@@ -15,5 +15,23 @@ describe("isValidCoordinateInput", () => {
 
   it("should return false if size have more than 2 spaces", () => {
     expect(isValidCoordinateInput("5 5 5")).toEqual(false);
+  });
+});
+
+describe("isValidInitialPositionInput", () => {
+  it("should return true if initial position is valid", () => {
+    expect(isValidInitialPositionInput("1 2 N")).toEqual(true);
+  });
+
+  it("should return false if position entered not having 2 spaces", () => {
+    expect(isValidInitialPositionInput("1 2 N N")).toEqual(false);
+  });
+
+  it("should return false if position does not start with a valid coordinates", () => {
+    expect(isValidInitialPositionInput("N 2 N")).toEqual(false);
+  });
+
+  it("should return false if position does not end with a valid orientation", () => {
+    expect(isValidInitialPositionInput("2 2 NH")).toEqual(false);
   });
 });
