dfeb433974a39e81aab81e2d6bc9726da68b2ec4
ngyl88 <ng_yl88@hotmail.com>
2018-08-20 15:13:20 +0800

    rover: print current position and (wip) process single instructions

--- src/Rover/Rover.js
+++ src/Rover/Rover.js
@@ -4,6 +4,16 @@ class Rover {
     this.y = y;
     this.orientation = orientation;
   }
+
+  printCurrentPosition() {
+    return `${this.x} ${this.y} ${this.orientation}`;
+  }
+
+  processInstruction(instruction) {
+    if (this.orientation === "N" && instruction === "L") {
+      this.orientation = "W";
+    }
+  }
 }
 
 module.exports = Rover;
--- src/Rover/Rover.test.js
+++ src/Rover/Rover.test.js
@@ -1,11 +1,35 @@
-const Rover = require('./Rover');
+const Rover = require("./Rover");
 
 it("Rover constructor", () => {
-    const rover = new Rover(0, 0, 'N');
-    expect(rover.x).toBe(0);
-    expect(rover.y).toBe(0);
-    expect(rover.orientation).toBe('N');
+  const rover = new Rover(0, 0, "N");
+  expect(rover.x).toBe(0);
+  expect(rover.y).toBe(0);
+  expect(rover.orientation).toBe("N");
 });
 
-describe.skip("L/R instructions", () => {
+it("printCurrentPosition()", () => {
+  const rover = new Rover(0, 0, "N");
+  expect(rover.printCurrentPosition()).toBe("0 0 N");
+});
+
+describe("Instructions", () => {
+  describe("L:", () => {
+    it("current orientation: N", () => {
+      let rover = new Rover(0, 0, "N");
+      rover.processInstruction("L");
+
+      expect(rover.printCurrentPosition()).toBe("0 0 W");
+    });
+    
+    it.skip("current orientation: S", () => {
+      let rover = new Rover(0, 0, "S");
+      rover.processInstruction("L");
+
+      expect(rover.printCurrentPosition()).toBe("0 0 E");
+    });
+  });
+
+  describe.skip("process R instructions", () => {});
+
+  describe.skip("process M instructions", () => {});
 });
