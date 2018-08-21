5352574a77400d07c8248ca98b1ab1fd882cceab
ngyl88 <ng_yl88@hotmail.com>
2018-08-21 23:58:54 +0800

    add missing tests for location.forward

--- src/location.test.js
+++ src/location.test.js
@@ -1,7 +1,41 @@
-const Location = require('./location');
+const Location = require("./location");
 
 it("location constructor", () => {
   const location = new Location(3, 5);
   expect(location.x).toBe(3);
   expect(location.y).toBe(5);
+});
+
+describe("forward", () => {
+  it("current orientation: N", () => {
+    const location = new Location(0, 0);
+    const newLocation = location.forward("N");
+
+    expect(newLocation.x).toBe(0);
+    expect(newLocation.y).toBe(1);
+  });
+
+  it("current orientation: S", () => {
+    const location = new Location(0, 1);
+    const newLocation = location.forward("S");
+
+    expect(newLocation.x).toBe(0);
+    expect(newLocation.y).toBe(0);
+  });
+
+  it("current orientation: E", () => {
+    const location = new Location(0, 0);
+    const newLocation = location.forward("E");
+
+    expect(newLocation.x).toBe(1);
+    expect(newLocation.y).toBe(0);
+  });
+
+  it("current orientation: W", () => {
+    const location = new Location(1, 0);
+    const newLocation = location.forward("W");
+
+    expect(newLocation.x).toBe(0);
+    expect(newLocation.y).toBe(0);
+  });
 });
