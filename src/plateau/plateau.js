cdcda9e2f93a3833ea54535653796edbee8e3bbe
ngyl88 <ng_yl88@hotmail.com>
2018-08-22 16:49:07 +0800

    addBeacon

--- src/plateau/plateau.js
+++ src/plateau/plateau.js
@@ -1,6 +1,13 @@
 class Plateau {
   constructor(boundaryLocation) {
     this.boundary = boundaryLocation;
+    this.beacons = [];
+  }
+
+  addBeacon(position) {
+    if (this.beacons.indexOf(position) === -1) {
+      this.beacons.push(position);
+    }
   }
 }
 
--- src/plateau/plateau.test.js
+++ src/plateau/plateau.test.js
@@ -8,3 +8,30 @@ it("Plateau constructor", () => {
   expect(plateau.boundary.x).toBe(5);
   expect(plateau.boundary.y).toBe(6);
 });
+
+describe("addBeacon()", () => {
+  let plateau = null;
+
+  beforeEach(() => {
+    const boundaryLocation = new Location(5, 6);
+    plateau = new Plateau(boundaryLocation);
+    expect(plateau.beacons.length).toBe(0);
+  });
+  
+  it('add if not exists', () => {
+    const position = "5 6 N";
+    plateau.addBeacon(position);
+
+    expect(plateau.beacons.length).toBe(1);
+    expect(plateau.beacons).toContain(position);
+  });
+
+  it('should not add if existing', () => {
+    const position = "5 6 N";
+    plateau.addBeacon(position);
+    plateau.addBeacon(position);
+
+    expect(plateau.beacons.length).toBe(1);
+    expect(plateau.beacons).toContain(position);
+  });
+});
